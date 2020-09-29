4e18217da77e3fe10c05d613474acfa8ae39592b
Eric Dam <dam.eric3@gmail.com>
2020-09-29 10:39:42 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Lab4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4.dir/main.cpp.o"
+  "Lab4.exe"
+  "Lab4.pdb"
+  "libLab4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
